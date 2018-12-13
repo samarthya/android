72c1722bff4f3952a99d4823532371558c4073d6
shasa50 <saurabh777@gmail.com>
2018-12-13 14:48:40 +0530

    Initial commit

---
+++ app/src/main/java/me/saurabh/samarthya/appone/ActivityOne.kt
@@ -0,0 +1,12 @@
+package me.saurabh.samarthya.appone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityOne : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one)
+    }
+}
